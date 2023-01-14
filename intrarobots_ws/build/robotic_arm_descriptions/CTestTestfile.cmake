afbab94fbc6a23fc35336dc55ce42af6831428c0
constant-inos <kon.karageorgos.g@gmail.com>
2023-01-14 14:40:48 +0200

    testing gitignore

--- intrarobots_ws/build/robotic_arm_descriptions/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ntinos/Workspaces/intrarobots_ws/src/robotic_arm_descriptions
-# Build directory: /home/ntinos/Workspaces/intrarobots_ws/build/robotic_arm_descriptions
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- intrarobots_ws/build/robotic_arm_moveit_interface/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ntinos/Workspaces/intrarobots_ws/src/robotic_arm_moveit_interface
-# Build directory: /home/ntinos/Workspaces/intrarobots_ws/build/robotic_arm_moveit_interface
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
