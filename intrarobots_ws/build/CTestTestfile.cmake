1f0e106b029b1ad7d8cd3a74dbac021387b7ff61
constant-inos <kon.karageorgos.g@gmail.com>
2023-02-24 21:09:15 +0200

    gianadoume

---
+++ intrarobots_ws/build/CTestTestfile.cmake
@@ -0,0 +1,16 @@
+# CMake generated Testfile for 
+# Source directory: /home/ntinos/Workspaces/intrarobots_ws/src
+# Build directory: /home/ntinos/Workspaces/intrarobots_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("robots/panda/panda_description")
+subdirs("robots/panda/panda_gazebo")
+subdirs("robots/panda/panda_moveit_config")
+subdirs("franka_description")
+subdirs("robotic_arm_descriptions")
+subdirs("robotic_arm_gazebo")
+subdirs("robotic_arm_moveit")
+subdirs("robotic_arm_moveit_interface")
+subdirs("master")
---
+++ intrarobots_ws/build/robots/panda/panda_gazebo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ntinos/Workspaces/intrarobots_ws/src/robots/panda/panda_gazebo
+# Build directory: /home/ntinos/Workspaces/intrarobots_ws/build/robots/panda/panda_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
